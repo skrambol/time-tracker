e38db278836089c81424f88b068dea35a58682b7
skrambol <systemplado@gmail.com>
2023-11-12 13:50:53 +0800

    added TimeTracking model

--- web/projects/models.py
+++ web/projects/models.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
 
 class Project(models.Model):
@@ -5,3 +7,13 @@ class Project(models.Model):
     description = models.CharField(max_length=255)
     date_start = models.DateTimeField()
     date_end = models.DateTimeField(blank=True, null=True)
+
+
+class TimeTracking(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    project = models.ForeignKey(Project, on_delete=models.PROTECT)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    date = models.DateField()
+    hours = models.IntegerField(validators=[MinValueValidator(1)])
+    description = models.CharField(max_length=255)
